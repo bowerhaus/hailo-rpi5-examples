baf7958ace8aee0185ba32a77946ad37f440174a
Bowerhaus <bowermail@gmail.com>
2025-02-10 10:58:13 +0000

    Use community projects folder for shell files

--- community_projects/watcher/start_watcher.sh
+++ community_projects/watcher/start_watcher.sh
@@ -3,6 +3,6 @@
 cd /home/bower/hailo-rpi5-examples
 source setup_env.sh
 cd ./community_projects/watcher
-python watcher.py --input rpi  > watcher.log 2>&1 &
+python watcher.py --input rpi  > ~/watcher.log 2>&1 &
 
 
--- community_projects/watcher/start_watcher_backup.sh
+++ community_projects/watcher/start_watcher_backup.sh
@@ -2,7 +2,7 @@
 
 cd /home/bower/hailo-rpi5-examples
 source setup_env.sh
-cd /home/bower/hailo-rpi5-examples/community_projects/watcher
+cd ./community_projects/watcher
 
 # Check if there is a process with the name "Hailo Detection"
 if pgrep -x "Hailo Detection" > /dev/null
@@ -10,6 +10,6 @@ then
     echo "Hailo process is already running."
 else
     echo "No Hailo process found. Starting the Python program."
-    python watcher.py --input rpi  > /home/bower/watcher.log 2>&1 &
+    python watcher.py --input rpi  > ~/watcher.log 2>&1 &
 fi
 
