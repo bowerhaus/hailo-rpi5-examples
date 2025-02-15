b2bbb72f18b83fb012173637ff918a918a629dae
Bowerhaus <bowermail@gmail.com>
2025-02-15 09:58:32 +0000

    Removed the tracker pipeline

--- community_projects/watcher/gstreamer_watcher_app.py
+++ community_projects/watcher/gstreamer_watcher_app.py
@@ -136,7 +136,7 @@ class GStreamerWatcherApp(GStreamerDetectionApp):
         pipeline_string = (
             f'{source_pipeline} ! '
             f'{detection_pipeline_wrapper} ! '
-            f'{tracker_pipeline} ! '
+
             f'{user_callback_pipeline} !'
             f'{display_pipeline}'
         )
