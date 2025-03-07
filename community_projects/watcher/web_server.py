a6c3c9de3a94c52dcc5f922e196cd102a7c143f1
Bowerhaus <bowermail@gmail.com>
2025-03-07 00:42:08 +0000

    Add login to HOM

--- community_projects/watcher/web_server.py
+++ community_projects/watcher/web_server.py
@@ -1,15 +1,17 @@
-from flask import Flask, send_from_directory, jsonify, request, send_file
+from flask import Flask, send_from_directory, jsonify, request, send_file, redirect, url_for
 import os
 import datetime
 import re
 import json
-import subprocess  # Import the subprocess module
-import tempfile  # Import the tempfile module
+import subprocess
+import tempfile
 from io import BytesIO
-from clock import Clock  # Import the Clock class
+from clock import Clock
+import jwt
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 OUTPUT_DIRECTORY = 'output'
+SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3R1c2VyQGV4YW1wbGUuY29tIiwiaWF0IjoxNzQxMzAzNTE0LCJleHAiOjE3NDEzMDcxMTR9.WSL53OdeTIlC1HT44_ZocGUTZf-nTm-GduDIKh-FEzo'
 
 def get_date_param():
     date = request.args.get('date')
@@ -17,7 +19,21 @@ def get_date_param():
         date = datetime.datetime.now().strftime("%Y%m%d")
     return date
 
+def token_required(f):
+    def wrap(*args, **kwargs):
+        token = request.cookies.get('token')
+        if not token:
+            return redirect(url_for('login_page'))
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            return redirect(url_for('login_page'))
+        return f(*args, **kwargs)
+    wrap.__name__ = f.__name__
+    return wrap
+
 @app.route('/api/files')
+@token_required
 def list_files():
     date = get_date_param()
     directory = os.path.join(OUTPUT_DIRECTORY, date)
@@ -29,6 +45,7 @@ def list_files():
     return jsonify(image_files)
 
 @app.route('/api/metadata')
+@token_required
 def list_metadata():
     date = get_date_param()
     directory = os.path.join(OUTPUT_DIRECTORY, date)
@@ -40,6 +57,7 @@ def list_metadata():
     return jsonify(json_files)
 
 @app.route('/api/images/<root_filename>')
+@token_required
 def list_sequence_images(root_filename):
     date = get_date_param()
     directory = os.path.join(OUTPUT_DIRECTORY, date)
@@ -50,6 +68,7 @@ def list_sequence_images(root_filename):
     return jsonify(sequence_images)
 
 @app.route('/api/update/<filename>', methods=['POST'])
+@token_required
 def update_json(filename):
     date = get_date_param()
     if not filename.endswith('.json'):
@@ -67,6 +86,7 @@ def update_json(filename):
         return jsonify({'error': str(e)}), 500
 
 @app.route('/api/delete/<base_filename>', methods=['DELETE'])
+@token_required
 def delete_files(base_filename):
     date = get_date_param()
     directory = os.path.join(OUTPUT_DIRECTORY, date)
@@ -78,8 +98,8 @@ def delete_files(base_filename):
     return jsonify({'success': True, 'deleted_files': files_to_delete})
 
 @app.route('/media/<filename>')
+@token_required
 def serve_media(filename):
-    # Extract the date from the start of the filename
     match = re.match(r'^(\d{8})_', filename)
     if not match:
         return jsonify({'error': 'Invalid filename format'}), 400
@@ -97,19 +117,19 @@ def serve_media(filename):
     return '', 404
 
 @app.route('/api/dates')
+@token_required
 def list_dates():
     if not os.path.exists(OUTPUT_DIRECTORY):
         return jsonify([])
 
     dates = [d for d in os.listdir(OUTPUT_DIRECTORY) if os.path.isdir(os.path.join(OUTPUT_DIRECTORY, d))]
-    dates.sort(reverse=True)  # Newest first
+    dates.sort(reverse=True)
     return jsonify(dates)
 
-# NEW: Add an API endpoint to generate and return a clock image
 @app.route('/api/clock_image')
+@token_required
 def get_clock_image():
     clock = Clock()
-    # Read days query parameter; default to all days if not provided
     days = request.args.get('days', "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday")
     pil_img = clock.create_clock_image(days=days)
     pil_img = clock.draw_current_hour_hand(pil_img)
@@ -120,6 +140,7 @@ def get_clock_image():
     return send_file(img_io, mimetype='image/png')
 
 @app.route('/api/cpu_temperature')
+@token_required
 def get_cpu_temp():
     try:
         temp = subprocess.check_output(["vcgencmd", "measure_temp"]).decode()
@@ -128,11 +149,31 @@ def get_cpu_temp():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username == 'bower' and password == 'R0oshARy':
+        token = jwt.encode({'username': username}, SECRET_KEY, algorithm='HS256')
+        response = jsonify({'token': token})
+        response.set_cookie('token', token)
+        return response
+    else:
+        return jsonify({'error': 'Invalid credentials'}), 401
+
+@app.route('/login')
+def login_page():
+    return send_from_directory('static', 'login.html')
+
 @app.route('/')
+@token_required
 def home():
     return send_from_directory('static', 'home.html')
 
 @app.route('/review')
+@token_required
 def review():
     return send_from_directory('static', 'index.html')
 
